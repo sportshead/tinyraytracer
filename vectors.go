7a895402160f08b8cd8c74808cd67d81326eeefb
sportshead <me@sportshead.dev>
2023-07-07 16:54:36 +0800

    vec2f

--- vectors.go
+++ vectors.go
@@ -5,6 +5,37 @@ import (
 	"math"
 )
 
+type Vec2f [2]float64
+
+func (v Vec2f) Add(other Vec2f) Vec2f {
+	return Vec2f{v[0] + other[0], v[1] + other[1]}
+}
+
+func (v Vec2f) Sub(other Vec2f) Vec2f {
+	return Vec2f{v[0] - other[0], v[1] - other[1]}
+}
+
+func (v Vec2f) Mul(f float64) Vec2f {
+	return Vec2f{v[0] * f, v[1] * f}
+}
+
+func (v Vec2f) Dot(other Vec2f) float64 {
+	return v[0]*other[0] + v[1]*other[1]
+}
+
+func (v Vec2f) Norm() float64 {
+	return math.Sqrt(v[0]*v[0] + v[1]*v[1])
+}
+
+func (v Vec2f) Normalize() Vec2f {
+	l := v.Norm()
+	return Vec2f{v[0] / l, v[1] / l}
+}
+
+func (v Vec2f) String() string {
+	return fmt.Sprintf("(%f, %f)", v[0], v[1])
+}
+
 type Vec3f [3]float64
 
 func (v Vec3f) Add(other Vec3f) Vec3f {
